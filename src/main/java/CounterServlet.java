89fc90189721886337448c9c319be1d8f2c4ced4
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-03-07 13:49:32 -0600

    counter exercise done

--- src/main/java/CounterServlet.java
+++ src/main/java/CounterServlet.java
@@ -1,2 +1,33 @@
-public class CounterServlet {
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "CounterServlet", urlPatterns = "/counter")
+public class CounterServlet extends HttpServlet {
+
+    private int counter = 0;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+        String reset = req.getParameter("reset");
+
+        if (reset != null && reset.equalsIgnoreCase("y")) {
+            counter = 1;
+        } else {
+            counter +=1;
+        }
+
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+
+        out.println("<h1>Counter: " + counter);
+
+    }
+
 }

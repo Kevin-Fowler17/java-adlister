76fd0c732d33bf44561fd1bdb9fae347a840e32d
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-03-09 13:36:11 -0600

    servlets with jsps exercise 1 done

--- src/main/java/PizzaOrderServlet.java
+++ src/main/java/PizzaOrderServlet.java
@@ -67,25 +67,4 @@ public class PizzaOrderServlet extends HttpServlet {
     }
 }
 
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//
-//        String crustType = request.getParameter("crustType");
-//        String sauceType = request.getParameter("sauceType");
-//        String sizeType = request.getParameter("sizeType");
-//        String[] toppings = request.getParameterValues("toppings");
-//        String deliveryAddress = request.getParameter("deliveryAddress");
-//
-//        System.out.println("Crust Type: " + crustType);
-//        System.out.println("Sauce Type: " + sauceType);
-//        System.out.println("Size Type: " + sizeType);
-//        System.out.println("Toppings: ");
-//        if (toppings != null) {
-//            for (String topping : toppings) {
-//                System.out.println("- " + topping);
-//            }
-//        }
-//        System.out.println("Delivery Address: " + deliveryAddress);
-//
-//        response.sendRedirect("pizza-order-confirmation.jsp");
-//    }
 
